2603e0c012b99a5b9328a23bb1dabd83c758a45e
N11Q <nii.q.ray@gmail.com>
2022-06-02 13:13:19 -0700

    C is fun!

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly the phrase "with proper grammar, but the outcome is a piece of art," 
+ * main - prints "with proper grammar, but the outcome is a piece of art," 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf ("with proper grammar but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }

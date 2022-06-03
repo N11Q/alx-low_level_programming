197bdeffe2ff20eac6cf87ba2054b04c447d4279
N11Q <nii.q.ray@gmail.com>
2022-06-03 12:07:28 -0700

    Taking things for a loop

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - assign a random number to the variable n each time it is executed,
+ *  print whether the number stored in n is positve or negative.
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+	      printf("%3d, is positive\n",n);
+	else
+		printf("%3d, is negative\n",n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,5 @@
+A fun collection of c files and scripts
+
+0-positive_or_negative.c - assigns a random number to the variable n each time it is executed, and print whether the number stored n is positive or negative.
+
+

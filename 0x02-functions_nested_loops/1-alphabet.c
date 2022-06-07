d9bb10bfa9d0b43f2f2c702a4ec3a890e2e71433
N11Q <nii.q.ray@gmail.com>
2022-06-07 16:00:04 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  * main -  prints the alphabet, in lowercase, followed by a new line.
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char a;
 	
@@ -14,5 +14,4 @@ int main(void)
 		_putchar(a);
 	}
 	_putchar('\n');
-	return (0);
 }

e51d00c66b696c72837c5319c069f9c18dbc2fd4
N11Q <nii.q.ray@gmail.com>
2022-06-08 07:12:11 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,23 +7,33 @@
  */
 int main(void)
 {
-	int a;
-	unsigned long int b, c, follows, sum;
+	unsigned long int a, b, c, b1, b2, c1, c2;
 
 	b = 1;
 	c = 2;
-	sum = 0;
-
-	for (a = 1; a <= 33; ++a)
+	
+	printf("%lu", b);
+	for (a = 1; a < 91; a++)
 	{
-		if (b < 4000000 && (b % 2) == 0)
-		{
-			sum = sum + b;
-		}
-		follows = b + c;
-		b = c;
-		c = follows;
+		printf(", %lu", c);
+		c = c + b;
+		c = c - b;
 	}
-	printf("%lu\n", sum);
+	
+	b1 = b / 100000000;
+	b2 = b / 100000000;
+	c1 = c / 100000000;
+	c2 = c / 100000000;
+
+	for (a = 92; a < 99; ++a)
+	{
+		printf(", %lu", c1 + (c2 / 1000000000));
+		printf("%lu", c2 % 1000000000);
+		c1 = c1 +  b1;
+		b1 = c1 - b1;
+		c2 = c2 + b2;
+		b2 = b2 - c2;
+	}
+	printf("\n");
 	return (0);
 }

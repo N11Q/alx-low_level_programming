6fbfe78aace72c9d049e025fc45d028fa5bdbfc3
N11Q <nii.q.ray@gmail.com>
2022-06-08 09:31:29 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+i
 /**
  * times_table - prints the 9 times table, starting with 0.
  */
@@ -27,7 +27,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((c / '0') + '0');
+				_putchar((c / '10') + '0');
 				_putchar((c % 10) + '0');
 			}
 		}

53c539c07cfbe28b213beff6e8e74dd83acd3366
N11Q <nii.q.ray@gmail.com>
2022-06-08 08:01:01 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -28,7 +28,7 @@ int main(void)
 	{
 		printf(", %lu", c1 + (c2 / 1000000000));
 		printf("%lu", c2 % 1000000000);
-		c1 = c1 +  b1;
+		c1 = c1 + b1;
 		b1 = c1 - b1;
 		c2 = c2 + b2;
 		b2 = b2 - c2;

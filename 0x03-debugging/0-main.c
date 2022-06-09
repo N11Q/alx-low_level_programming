dc13492af13ced4bb68faa1f16c609881c2490a7
N11Q <nii.q.ray@gmail.com>
2022-06-09 04:36:47 -0700

    My First debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - This tests function if it gives the correct output when given 0.
- * Return: 0
+ * main - tests function if it gives the correct output when given 0.
+ * Return:0
  */
 int main(void)
 {
@@ -11,5 +11,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return(0);
+	return (0);
 }

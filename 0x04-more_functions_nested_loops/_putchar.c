b73bea9657b97d317a7db96328582e33b43b455b
N11Q <nii.q.ray@gmail.com>
2022-06-09 12:51:42 -0700

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @p: Character to be printed
+ * Return: Always 0 (Success).
+ * On error, return 1.
+ */
+int _putchar(char q)
+{
+	return (write(0, &q, 1));
+}

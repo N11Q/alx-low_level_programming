0622ff5c01edfd65360b5a4fdac1813368d977a5
N11Q <nii.q.ray@gmail.com>
2022-06-09 04:08:30 -0700

    My First debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main - tests positive_or_negative() if it gives the correct output when given 0
  * Return: 0
  */
 

55c8a2f254d7e9877385a037d83d2b837c47390f
N11Q <nii.q.ray@gmail.com>
2022-06-09 16:10:50 -0700

    14x10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,6 @@
 
 /*
  * more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line.
- * Return: Always 0 (Success)
  */
 void more_numbers(void)
 {
@@ -16,6 +15,6 @@ void more_numbers(void)
 				_putchar(b / 10 + '0');
 			_putchar(b % 10 + '0');
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }

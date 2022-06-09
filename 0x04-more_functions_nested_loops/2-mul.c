7fa657b069a6318ac661a08acbf3eac472041b69
N11Q <nii.q.ray@gmail.com>
2022-06-09 12:30:38 -0700

    Let them multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * mul - function that multiplies two integers.
+ * @a - int going to be multiplied 
+ * @b - int going to be multiplied
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int c;
+	
+	c = a * b;
+	
+	return (c);
+}

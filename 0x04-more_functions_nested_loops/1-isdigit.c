05947db9473a5a5926ce3a5bc30e3da0830b95c2
N11Q <nii.q.ray@gmail.com>
2022-06-09 08:03:04 -0700

    only digits allowed here

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - that checks for a digit (0 through 9).
+ * @c is the character checked
+ * Return: 1 if c is a digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c = 1 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}

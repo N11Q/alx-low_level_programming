19f8e0293a5e9578c38fd6e9e3f57843db534ec2
N11Q <nii.q.ray@gmail.com>
2022-06-13 14:54:10 -0700

    Printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ * Return: Aliii ways 0
+ */
+int main(void)
+{
+	chat *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}

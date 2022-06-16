7854ab4a7211074803a0be4163b9d6981a10af0e
N11Q <nii.q.ray@gmail.com>
2022-06-16 12:35:09 -0700

    Comparing strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * strcmp - compares two strings
+ * _strcmp - compares two strings
  * @s1: 1st string to compare
  * @s2: 2nd string to comprare
  * Return: 0 if equal
  * less than zero if s1 < s2
  * greater than zero if s1 > s2
  */
-int _strcmp(*char s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
-		if (s1 == '\0')
+		if (*s1 == '\0')
 		{
 			return (0);
 		}
